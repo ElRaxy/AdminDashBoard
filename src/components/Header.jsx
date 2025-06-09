195f75cb40de57a4b102ada8e5493bd8104b87a5
Alex Mico <alexmico2006@gmail.com>
2025-06-09 09:43:52 +0200

    9/06/2025-9:43

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,25 +3,11 @@ import '../css/header.css'
 import Logo from './Logo';
 import SearchBar from './SearchBar';
 import Nav from './Nav';
-import { useTheme } from '../contexts/ThemeContext';
 
 function Header() {
-  const { darkMode, toggleTheme } = useTheme();
-
-  const toggleSidebar = () => {
-    document.body.classList.toggle('toggle-sidebar');
-    const sidebar = document.querySelector('.sidebar');
-    if (sidebar) {
-      sidebar.classList.toggle('active');
-    }
-  }
-
   return (
     <header id='header' className='header fixed-top d-flex align-items-center'>
-      <div className="d-flex align-items-center justify-content-between">
-        <Logo />
-        <i className='bi bi-list toggle-sidebar-btn' onClick={toggleSidebar}></i>
-      </div>
+      <Logo />
       <SearchBar />
       <Nav />
     </header>
