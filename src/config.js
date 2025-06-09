8bb7ca7d79f372df0f4b323ce29e106dfa4efa87
Alex Mico <alexmico2006@gmail.com>
2025-06-09 11:13:14 +0200

    Fetch Info Commit

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const isDevelopment = process.env.NODE_ENV === 'development';
 
 const config = {
-    apiUrl: isDevelopment ? 'http://localhost:4000/api' : '',
+    apiUrl: isDevelopment ? 'http://localhost:4000' : 'https://admin-dash-board-alex.vercel.app/api',
 };
 
 export default config; 
