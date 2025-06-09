c4a34336ca057136dbd354184059c7a5cd365de7
Alex Mico <alexmico2006@gmail.com>
2025-06-09 11:56:07 +0200

    Last Commit

--- api/middleware.js
+++ api/middleware.js
@@ -3,11 +3,8 @@ const path = require('path');
 const express = require('json-server/node_modules/express');
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'info.json'));
-const middlewares = jsonServer.defaults({
-    static: path.join(__dirname)
-});
 
-// Añadir headers CORS
+// Configurar CORS
 server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', '*');
@@ -15,18 +12,30 @@ server.use((req, res, next) => {
     next();
 });
 
+// Servir archivos estáticos desde la carpeta img
+server.use('/api/img', express.static(path.join(__dirname, 'img'), {
+    setHeaders: (res) => {
+        res.set('Access-Control-Allow-Origin', '*');
+        res.set('Cache-Control', 'public, max-age=31536000');
+    }
+}));
+
+// Usar los middlewares por defecto de json-server
+server.use(jsonServer.defaults());
+
+// Añadir prefijo /api a todas las rutas del router
+server.use('/api', (req, res, next) => {
+    if (req.url.startsWith('/img/')) {
+        next();
+    } else {
+        router(req, res, next);
+    }
+});
+
 // Manejar errores
 server.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ error: 'Something broke!' });
 });
-
-server.use(middlewares);
-
-// Servir archivos estáticos desde la carpeta img
-server.use('/api/img', express.static(path.join(__dirname, 'img')));
-
-// Añadir prefijo /api a todas las rutas del router
-server.use('/api', router);
 
 module.exports = server; 
